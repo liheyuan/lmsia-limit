bcc70574ceee6f16404fbf4434cdbfe66d41e19a
coder4 <lihy@fenbi.com>
2018-09-29 15:43:19 +0800

    FIX: comment

--- src/main/java/com/coder4/lmsia/ratelimit/configuration/RateLimitConfiguration.java
+++ src/main/java/com/coder4/lmsia/ratelimit/configuration/RateLimitConfiguration.java
@@ -1,9 +1,3 @@
-/**
- * @(#)RateLimitConfiguration.java, Sep 29, 2018.
- * <p>
- * Copyright 2018 fenbi.com. All rights reserved.
- * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.coder4.lmsia.ratelimit.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
